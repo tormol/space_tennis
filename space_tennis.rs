95e8ecbd88b328c3dfcd983e9520e26e6f696f02
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2017-06-16 20:38:09 +0200

    compiles with relatively recent depeendencies

--- space_tennis.rs
+++ space_tennis.rs
@@ -1,4 +1,4 @@
-/* Copyright 2016 Torbjørn Birch Moltu
+/* Copyright 2017 Torbjørn Birch Moltu
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,24 +11,34 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 use std::ops::Neg;
 extern crate num;
 use num::{Zero,One,ToPrimitive};
 extern crate vecmath;
-use vecmath::vec2_add;// Vector2 is [T; 2]
-extern crate graphics;
-use graphics::{Context,DrawState,Transformed,color,math};
-use graphics::types::Color;
-extern crate piston;
-use piston::input::keyboard::Key;
-use piston::input::mouse::MouseButton;
+use vecmath::vec2_add; // Vector2 is [T; 2]
+// Why not +?
+// Any math library in rust have to make a choice: Either use primitive slices or tuples
+// which make constructing and destructuring pain-free, or use std::ops::*.
+// Rusts coherence rules prevents them for doing both:
+// You cannot implement an external trait for a foreign type.
+extern crate piston_window;
+use piston_window::{Context,DrawState,Transformed,color,math}; // from piston2d-graphics
+use piston_window::types::Color; // from piston2d-graphics
+
+use std::cmp;
+use std::collections::vec_deque::VecDeque;
+ // from piston::input:
+use piston_window::keyboard::Key;
+use piston_window::mouse::MouseButton;
 extern crate opengl_graphics;
 use opengl_graphics::GlGraphics;
 use opengl_graphics::glyph_cache::GlyphCache;
-
+extern crate rand;
+use rand::Rng;
 
 const INITIAL_SIZE: [f64;2] = [500.0, 500.0];
 const UPDATE_TIME: f64 = 1.0/60.0;
@@ -80,32 +90,32 @@ impl Game {
     }
 }
 
-
-use piston::window::WindowSettings;
-use piston::event_loop::{Events,WindowEvents};
-use piston::input::{Button, Motion, Event, Input};
 use opengl_graphics::OpenGL;
-use graphics::draw_state::Blend;
+use piston_window::{Input,Button,Motion,RenderArgs,UpdateArgs}; // from piston::input
+use piston_window::draw_state::Blend; // from piston2d-graphics
+use piston_window::WindowSettings; // from piston::window
+use piston_window::Events; // from piston::event_loop
+use piston_window::PistonWindow; // from piston_window
 
-extern crate piston_window;
-use piston_window::PistonWindow;
+use std::time::Instant;
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("space tennis", [INITIAL_SIZE[0] as u32,
-                                             INITIAL_SIZE[1] as u32])
-                       .exit_on_esc(true)
-                       .build().unwrap();
-   let mut gfx = GlGraphics::new(OpenGL::V3_2);
+    let mut window: PistonWindow = WindowSettings::new(
+            "space tennis", [INITIAL_SIZE[0] as u32, INITIAL_SIZE[1] as u32]
+        ).exit_on_esc(true)
+        .opengl(OpenGL::V3_2)
+        .build()
+        .unwrap();
+    let mut gfx = GlGraphics::new(OpenGL::V3_2);
 
     let mut size = INITIAL_SIZE;//changes if window is resized
     let mut offset = [0.0; 2];//letterboxing after resize
 
     let mut game = Game::new();
-    let mut event_loop: WindowEvents = window.events();
+    let mut event_loop: Events = window.events;
     while let Some(e) = event_loop.next(&mut window) {
         match e {
-            Event::Render(render_args/*: RenderArgs*/) => {
+            Input::Render(render_args/*: RenderArgs*/) => {
                 let context: Context = Context::new_viewport(render_args.viewport())
                                                .trans(offset[0], offset[1])
                                                .scale(size[0], size[1]);
@@ -113,14 +123,15 @@ fn main() {
                 //since colors are blended pixel for pixel, this has a performance cost,
                 //the alternative is to check for existing color in tile, and blend manually, or even statically
                 context.draw_state.blend(Blend::Alpha);
+                piston_window::clear(color::BLACK, &mut gfx);
 
                 game.render(context.draw_state, context.transform, &mut gfx);
             }
-            Event::Update(update_args) => {
+            Input::Update(update_args) => {
                 game.update(update_args.dt);// deltatime is its only field
             }
 
-            Event::Input(Input::Resize(x,y)) => {
+            Input::Resize(x,y) => {
                 let min = f64::min(x as f64 / INITIAL_SIZE[0],
                                    y as f64 / INITIAL_SIZE[1]);
                 size = [INITIAL_SIZE[0]*min, INITIAL_SIZE[1]*min];
@@ -129,11 +140,11 @@ fn main() {
                 gfx.viewport(0, 0, x as i32, y as i32);
             }
 
-            Event::Input(Input::Press(Button::Keyboard(key))) => {
+            Input::Press(Button::Keyboard(key)) => {
                 game.key_press(key);
             }
-            Event::Input(Input::Move(Motion::MouseCursor(x,y))) => {
-                let mut pos = None;
+            Input::Move(Motion::MouseCursor(x,y)) => {
+                let mut pos: Option<[f64; 2]> = None;
                 let x = (x - offset[0]) / size[0];
                 let y = (y - offset[1]) / size[1];
                 if x >= 0.0  &&  x < 1.0
@@ -142,7 +153,7 @@ fn main() {
                 }
                 game.mouse_move(pos);
             }
-            Event::Input(Input::Cursor(_)) => {//only happens if a button is pressed
+            Input::Cursor(_) => {//only happens if a button is pressed
                 game.mouse_move(None);
             }
             _ => {}
